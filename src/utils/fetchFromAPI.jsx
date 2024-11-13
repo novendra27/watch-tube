4925c18250426b1d7f83c65c2c63b65ddf7c7d44
novendra27 <adi.novendra.p@gmail.com>
2024-11-13 17:51:53 +0700

    Migration to VITE framework

--- src/utils/fetchFromAPI.jsx
+++ src/utils/fetchFromAPI.jsx
@@ -8,7 +8,7 @@ const options = {
         maxResults: '50'
     },
     headers: {
-        'x-rapidapi-key': process.env.REACT_APP_RAPID_API_KEY,
+        'x-rapidapi-key': import.meta.env.VITE_RAPID_API_KEY,
         'x-rapidapi-host': 'youtube-v31.p.rapidapi.com'
     }
 };
